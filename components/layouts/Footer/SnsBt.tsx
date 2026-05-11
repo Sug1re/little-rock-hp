e7db4f78143c045bf8be6c318ecc6813148cd19d
Sug1re <sugirex@icloud.com>
2026-05-11 12:17:44 +0900

    SNSのリンクを追加

--- components/layouts/Footer/SnsBt.tsx
+++ components/layouts/Footer/SnsBt.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Box, Button } from "@mui/material";
 import {
   faInstagram,
+  faTiktok,
   faXTwitter,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
@@ -14,7 +15,7 @@ export const SnsBt = () => {
       <Box sx={{ display: "flex", justifyContent: "center", gap: 2 }}>
         <Button
           component={Link}
-          href="https://youtube.com/your_account"
+          href="https://www.youtube.com/@little__rock_"
           target="_blank"
           rel="noopener noreferrer"
           sx={{ m: 0, p: 0, minWidth: 0, lineHeight: 1 }}
@@ -23,7 +24,7 @@ export const SnsBt = () => {
         </Button>
         <Button
           component={Link}
-          href="https://instagram.com/your_account"
+          href="https://www.instagram.com/little_._._rock?igsh=emRnZTZxN2Fwc3k5"
           target="_blank"
           rel="noopener noreferrer"
           sx={{ m: 0, p: 0, minWidth: 0, lineHeight: 1 }}
@@ -32,12 +33,21 @@ export const SnsBt = () => {
         </Button>
         <Button
           component={Link}
-          href="https://twitter.com/your_account"
+          href="https://x.com/little__rock_?s=21&t=udQAt5SX7nQssa8CRpBkYg"
           target="_blank"
           rel="noopener noreferrer"
           sx={{ m: 0, p: 0, minWidth: 0, lineHeight: 1 }}
         >
           <FontAwesomeIcon icon={faXTwitter} style={{ fontSize: "28px" }} />
+        </Button>
+        <Button
+          component={Link}
+          href="https://www.tiktok.com/@little_._._rock?_r=1&_t=ZS-96BhP4aXqmj"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{ m: 0, p: 0, minWidth: 0, lineHeight: 1 }}
+        >
+          <FontAwesomeIcon icon={faTiktok} style={{ fontSize: "28px" }} />
         </Button>
       </Box>
     </>
